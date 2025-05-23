17de318cd475a4626d0a688e7cea2003d7b96cef
lium-dot <allenkangethe921@gmail.com>
2025-05-23 22:57:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,31 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "BMB-XMD~vFAxTbJI#dEsYrYebvzEn_f5fg2_byNNKntlNkfS-UdfoCjn4PR4",
+SESSION_ID: process.env.SESSION_ID || "BMB-XMD~vRBFXJhQ#9OC0ql20cBaZbFOJ5UfnOS8RWVNmmqdnyzSZVQT-7ow",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY B.M.B-XMD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*ᏋᏤᏋRᎽTHIᏁᎶ IᎦ Ꮨ ᎠᏋᎦTRUᏨTIᎧᏁ ᏠUᎦT FᎧRᏨURᎦᏋ ᎧᏁ ᎽᎧUR ᎶᎧᏘL ",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
+WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/6lwftg.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "*",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "NEXUS-XMD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "NEXUS-XMD",
+STICKER_NAME: process.env.STICKER_NAME || "MR ΛĿĿƐИ",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,11 +37,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255767862457",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254758443111",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝙱.𝙼.𝙱-𝚇𝙼𝙳",
+OWNER_NAME: process.env.OWNER_NAME || MR ΛĿĿƐИ",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© powered by 𝙱.𝙼.𝙱-𝚇𝙼𝙳*",
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by MR ΛĿĿƐИ*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6lwftg.jpg",
 // add img for alive msg
@@ -63,22 +63,22 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
+READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255767862457",
+DEV: process.env.DEV || "254758443111",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding
-ANTICALL: process.env.ANTICALL || "false"
+ANTICALL: process.env.ANTICALL || "true"
 // make it true for auto recoding 
 };    
